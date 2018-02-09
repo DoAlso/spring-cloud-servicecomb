3a1d84338acf2463432ebcbaf047ee0223853c71
admin <315238976@qq.com>
2018-02-09 18:14:16 +0800

    添加微服务注册到servicecomb本地注册中心配置。

--- service-base/base-api/src/main/java/com/servicecomb/base/BaseApiApplication.java
+++ service-base/base-api/src/main/java/com/servicecomb/base/BaseApiApplication.java
@@ -1,9 +1,13 @@
 package com.servicecomb.base;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringCloudApplication
+@EnableAutoConfiguration
+@ImportResource(locations = "classpath*:META-INF/spring/*.bean.xml")
 public class BaseApiApplication {
 
 	public static void main(String[] args) {
--- service-gateway/gateway-build/src/main/java/com/servicecomb/gateway/GatewayBuildApplication.java
+++ service-gateway/gateway-build/src/main/java/com/servicecomb/gateway/GatewayBuildApplication.java
@@ -1,9 +1,15 @@
 package com.servicecomb.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@EnableZuulProxy
+@SpringCloudApplication
+@EnableAutoConfiguration
+@ImportResource(locations = "classpath*:META-INF/spring/*.bean.xml")
 public class GatewayBuildApplication {
 
 	public static void main(String[] args) {
