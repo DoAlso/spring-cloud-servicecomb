2ae7f5c069d786e7edbd67a7081337c2c9b1b2d2
胡亚曦 <hyx199170@163.com>
2019-01-25 17:25:17 +0800

    完善框架demo

--- sample-consumer/src/main/java/com/sample/servicecomb/consumer/ConsumerApplication.java
+++ sample-consumer/src/main/java/com/sample/servicecomb/consumer/ConsumerApplication.java
@@ -3,7 +3,8 @@ package com.sample.servicecomb.consumer;
 import org.apache.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
 import org.apache.servicecomb.springboot2.starter.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,8 +14,9 @@ import org.springframework.web.client.RestTemplate;
  * @Author Administrator
  * @DATE 2019/1/21 17:07
  */
+@EnableDiscoveryClient
 @EnableServiceComb
-@SpringCloudApplication
+@SpringBootApplication
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class,args);
