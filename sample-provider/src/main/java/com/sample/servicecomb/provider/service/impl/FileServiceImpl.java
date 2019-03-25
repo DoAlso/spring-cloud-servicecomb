547b068ce08a31b6b88b507ec02878a6222a1969
胡亚曦 <hyx199170@163.com>
2019-03-25 15:03:30 +0800

    提交修改

--- sample-provider/src/main/java/com/sample/servicecomb/provider/service/impl/FileServiceImpl.java
+++ sample-provider/src/main/java/com/sample/servicecomb/provider/service/impl/FileServiceImpl.java
@@ -41,26 +41,31 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public ResponseEntity claimUploadId(String fileName) throws Exception {
-        String claimUploadId = obsClientBuilder.claimUploadId(ConstantsUtil.OBS.BUCKET_NAME,fileName);
-        return ResponseEntityUtil.response("ok","0000",claimUploadId);
+        String claimUploadId = obsClientBuilder.claimUploadId(ConstantsUtil.OBS.BUCKET_NAME, fileName);
+        return ResponseEntityUtil.response("ok", "0000", claimUploadId);
     }
 
     @Override
-    public ResponseEntity upload(MultipartFile multipartFile,String fileName,Integer partNum, String uploadId) throws Exception{
-        PartEtag partEtag = obsClientBuilder.uploadPart(multipartFile,ConstantsUtil.OBS.BUCKET_NAME,fileName,partNum,uploadId);
-        return ResponseEntityUtil.response("ok","0000",partEtag);
+    public ResponseEntity upload(MultipartFile multipartFile, String fileName, Integer partNum, String uploadId)
+            throws Exception {
+        PartEtag partEtag = obsClientBuilder.uploadPart(multipartFile, ConstantsUtil.OBS.BUCKET_NAME, fileName, partNum,
+                uploadId);
+        return ResponseEntityUtil.response("ok", "0000", partEtag);
     }
 
     @Override
-    public ResponseEntity completeMultipartUpload(String uploadId, String objectKey, List<PartEtag> partETags) throws Exception {
-        CompleteMultipartUploadResult uploadResult = obsClientBuilder.completeMultipartUpload(uploadId,ConstantsUtil.OBS.BUCKET_NAME,objectKey,partETags);
-        return ResponseEntityUtil.response("ok","0000",uploadResult);
+    public ResponseEntity completeMultipartUpload(String uploadId, String objectKey, List<PartEtag> partETags)
+            throws Exception {
+        CompleteMultipartUploadResult uploadResult = obsClientBuilder.completeMultipartUpload(uploadId,
+                ConstantsUtil.OBS.BUCKET_NAME, objectKey, partETags);
+        return ResponseEntityUtil.response("ok", "0000", uploadResult);
     }
 
     @Override
     public ResponseEntity sampleUpload(MultipartFile file) throws Exception {
-        String fileName = "faces/2/"+file.getOriginalFilename();
-        PutObjectResult result = obsClientBuilder.putObject(ConstantsUtil.OBS.BUCKET_NAME,fileName,file.getInputStream());
+        String fileName = "faces/2/" + file.getOriginalFilename();
+        PutObjectResult result = obsClientBuilder.putObject(ConstantsUtil.OBS.BUCKET_NAME, fileName,
+                file.getInputStream());
         BaseObs baseObs = new BaseObs();
         baseObs.setBucketName(result.getBucketName());
         baseObs.setObjectKey(result.getObjectKey());
@@ -75,50 +80,49 @@ public class FileServiceImpl implements FileService {
         baseObs.setVersionId(result.getVersionId());
         baseObs.setEtag(result.getEtag());
         baseObsMapper.insertSelective(baseObs);
-        return ResponseEntityUtil.response("ok","0000",baseObs);
+        return ResponseEntityUtil.response("ok", "0000", baseObs);
     }
 
     @Override
     public ResponseEntity createBucket(CreateBucketVO createBucketReq) throws Exception {
         ObsBucket bucket = obsClientBuilder.createBucket(createBucketReq.getBucketName());
-        return ResponseEntityUtil.response("ok","0000",bucket);
-    }
-
-
-    @Override
-    public FilePart download(String bucketName, Map<String,String> map) throws Exception {
-        String temPath = Files.createTempDirectory("temp").toFile().getAbsolutePath()+"\\";
-        String path = temPath+CommonUtil.generateRandomFilePath();
-        map.forEach((key,value) -> obsClientBuilder.downloadObject(bucketName,value,path+key));
-        String zipFile = temPath+CommonUtil.generateRandomFilename("zip");
-        FileTools.zipMultiFile(path,zipFile ,false);
-        return new FilePart("",zipFile).setDeleteAfterFinished(true);
+        return ResponseEntityUtil.response("ok", "0000", bucket);
     }
 
     @Override
-    public FilePart func(String bucketName, Map<String,String> map){
-        try{
-            String temPath = Files.createTempDirectory("temp").toFile().getAbsolutePath()+"\\";
-            String zipFile = temPath+CommonUtil.generateRandomFilename("zip");
+    public FilePart download(String bucketName, Map<String, String> map) throws Exception {
+        String temPath = Files.createTempDirectory("temp").toFile().getAbsolutePath() + "\\";
+        String path = temPath + CommonUtil.generateRandomFilePath();
+        map.forEach((key, value) -> obsClientBuilder.downloadObject(bucketName, value, path + key));
+        String zipFile = temPath + CommonUtil.generateRandomFilename("zip");
+        FileTools.zipMultiFile(path, zipFile, false);
+        return new FilePart("", zipFile).setDeleteAfterFinished(true);
+    }
+
+    @Override
+    public FilePart func(String bucketName, Map<String, String> map) {
+        try {
+            String temPath = Files.createTempDirectory("temp").toFile().getAbsolutePath() + "\\";
+            String zipFile = temPath + CommonUtil.generateRandomFilename("zip");
             ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile));
-            map.forEach((key,value)->{
-                ObsObject obsObject = obsClientBuilder.getObsObject(bucketName,value);
+            map.forEach((key, value) -> {
+                ObsObject obsObject = obsClientBuilder.getObsObject(bucketName, value);
                 byte[] buf = new byte[1024];
                 InputStream inputStream = obsObject.getObjectContent();
                 try {
                     zipOut.putNextEntry(new ZipEntry(key));
                     int len;
-                    while((len = inputStream.read(buf)) != -1){
+                    while ((len = inputStream.read(buf)) != -1) {
                         zipOut.write(buf, 0, len);
                     }
                     inputStream.close();
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             });
             zipOut.close();
-            return new FilePart("",zipFile).setDeleteAfterFinished(true);
-        }catch (Exception e){
+            return new FilePart("", zipFile).setDeleteAfterFinished(true);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
