a13c03c4aa2ff47b4dd9633d3a505206cc172fb8
胡亚曦 <hyx199170@163.com>
2019-05-23 14:48:02 +0800

    提交代码

--- sample-sdk/src/main/java/com/sample/servicecomb/common/configuration/SdkCoreConfiguration.java
+++ sample-sdk/src/main/java/com/sample/servicecomb/common/configuration/SdkCoreConfiguration.java
@@ -5,8 +5,6 @@ import com.sample.servicecomb.common.frs.FrsClientUtil;
 import com.sample.servicecomb.common.obs.ObsClientUtil;
 import com.sample.servicecomb.common.smn.SmnClientUtil;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,12 +14,10 @@ import org.springframework.context.annotation.Configuration;
  * @DATE 2019/5/23 13:35
  */
 @Configuration
-@EnableConfigurationProperties(SdkCoreProperties.class)
 public class SdkCoreConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(prefix = "sdk",name = "smnEnable",havingValue = "true")
     public SmnClientUtil getSmnClientUtil(SmnConfigurationProperties properties){
         SmnClientUtil smnClientUtil = new SmnClientUtil();
         smnClientUtil.setProperties(properties);
@@ -32,7 +28,6 @@ public class SdkCoreConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(prefix = "sdk",name = "disEnable",havingValue = "true")
     public DisClientUtil getDisClientUtil(DisConfigurationProperties properties){
         DisClientUtil disClientUtil = new DisClientUtil();
         disClientUtil.setDisProperties(properties);
@@ -43,7 +38,6 @@ public class SdkCoreConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(prefix = "sdk",name = "frsEnable",havingValue = "true")
     public FrsClientUtil getFrsClientUtil(FrsConfigurationProperties properties) throws Exception{
         FrsClientUtil frsClientUtil = new FrsClientUtil();
         frsClientUtil.setFrsProperties(properties);
@@ -54,7 +48,6 @@ public class SdkCoreConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(prefix = "sdk",name = "obsEnable",havingValue = "true")
     public ObsClientUtil getObsClientUtil(ObsConfigurationProperties properties){
         ObsClientUtil obsClientUtil = new ObsClientUtil();
         obsClientUtil.setObsProperties(properties);
